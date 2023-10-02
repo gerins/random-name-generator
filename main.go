870e35cd219ae88cc8657162410ab6f76132c4a6
garin_prodigi <gerin.rin@gmail.com>
2023-10-03 00:42:21 +0700

    use external package for randoming person data

--- main.go
+++ main.go
@@ -3,41 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"math/rand"
 	"net/http"
 
+	"github.com/Pallinder/go-randomdata"
 	"github.com/labstack/echo/v4"
 )
 
 type Person struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-}
-
-var firstNames = []string{
-	"John",
-	"Jane",
-	"Alice",
-	"Bob",
-	"Emma",
-	"David",
-	"Grace",
-	"Harry",
-	"Olivia",
-	"Peter",
-}
-
-var lastNames = []string{
-	"Smith",
-	"Johnson",
-	"Brown",
-	"Taylor",
-	"Davis",
-	"Wilson",
-	"Lee",
-	"Jones",
-	"Garcia",
-	"Miller",
+	Email     string `json:"email"`
+	Country   string `json:"country"`
+	City      string `json:"city"`
 }
 
 func main() {
@@ -48,8 +25,11 @@ func main() {
 
 	e.GET("/api/person", func(c echo.Context) error {
 		person := Person{
-			FirstName: firstNames[rand.Intn(len(firstNames))],
-			LastName:  lastNames[rand.Intn(len(lastNames))],
+			FirstName: randomdata.FirstName(randomdata.RandomGender),
+			LastName:  randomdata.LastName(),
+			Email:     randomdata.Email(),
+			Country:   randomdata.Country(randomdata.FullCountry),
+			City:      randomdata.City(),
 		}
 		return c.JSON(http.StatusOK, person)
 	})
